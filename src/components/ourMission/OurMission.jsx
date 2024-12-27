b278119712c7d2319228315725ff17d6c384a102
sarimsaleem <sarimsaleem07@gmail.com>
2024-12-27 21:58:53 +0500

    changes

--- src/components/ourMission/OurMission.jsx
+++ src/components/ourMission/OurMission.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./ourMission.css"
-import bgi from "../../assets/mission.png"
+import bgi from "../../assets/ourmission.jpg"
 const OurMission = () => {
     return (
         <div className='mision-parent'>
             <div className='mision-subparent'>
                 <img src={bgi} alt="" />
                 <div className="mission-text-parent">
-                    <h3>Our Mission</h3>
+                    <h3 className='component-heading' style={{color:"white"}}>Our Mission</h3>
                     <p>
                         Our mission is to provide tailored consulting services that drive meaningful impact.
                         Our vision is to become a trusted partner for businesses navigating complex challenges.
